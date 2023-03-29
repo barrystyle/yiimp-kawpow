05b1ae3a0736376550248cf56bf7fbde59a6a6f5
barrystyle <barrystyle@westnet.com.au>
2023-03-29 20:12:04 +0800

    add some copyrights

--- firopow/overrides.cpp
+++ firopow/overrides.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 barrystyle
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "stratum.h"
 
 const int get_ethash_epoch_length()
--- kawpow/kawpow.cpp
+++ kawpow/kawpow.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 barrystyle
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "stratum.h"
 
 #include <openssl/sha.h>
